8e5e11941865f0475d544523c961fa8f20902457
Sarma Krovi <sarmakdvsr@gmail.com>
2021-03-22 21:55:12 -0400

    list tables

--- TablePartB.java
+++ TablePartB.java
@@ -1,34 +1,26 @@
 import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
-
-import org.apache.hadoop.hbase.TableName;
-
 import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
 
-import org.apache.hadoop.hbase.util.Bytes;
+public class TablePartB {
 
-public class TablePartB{
+  public static void main(String[] args) throws IOException {
 
-   public static void main(String[] args) throws IOException {
+    // Instantiating configuration class
+    Configuration con = HBaseConfiguration.create();
 
-   	HTableDescriptor[] tableDescriptor = null; //???;
-	//TODO
- /*
-	for(???){
-         System.out.println(tableDescriptor[i].getNameAsString());
-     }
+    // Instantiating HbaseAdmin class
+    try (HBaseAdmin admin = new HBaseAdmin(con)) {
 
-  */
-   }
+
+      HTableDescriptor[] tableDescriptor = admin.listTables();
+      for (int i = 0; i < tableDescriptor.length; i++) {
+        System.out.println(tableDescriptor[i].getNameAsString());
+      }
+
+    }
+  }
 }
 
