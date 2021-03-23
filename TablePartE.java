5acdade1b8e53d21f842c2a8629cae3fcb8402a9
Sarma Krovi <sarmakdvsr@gmail.com>
2021-03-22 22:16:30 -0400

    TablePartE

--- TablePartE.java
+++ TablePartE.java
@@ -1,32 +1,24 @@
 import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-
-import org.apache.hadoop.hbase.TableName;
-
-import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 
-import org.apache.hadoop.hbase.util.Bytes;
+@SuppressWarnings("deprecation")
+public class TablePartE {
 
-public class TablePartE{
+  public static void main(String[] args) throws IOException {
+    Configuration config = HBaseConfiguration.create();
+    HTable table = new HTable(config, "powers");
+    Scan scan = new Scan();
 
-   public static void main(String[] args) throws IOException {
-
-	// TODO      
-	// DON' CHANGE THE 'System.out.println(xxx)' OUTPUT PART
-	// OR YOU WON'T RECEIVE POINTS FROM THE GRADER
-	ResultScanner scanner = null; // ???;
-	for (Result result = scanner.next(); result != null; result = scanner.next())
-		System.out.println(result);
-   }
+    try (ResultScanner scanner = table.getScanner(scan)) {
+      for (Result result = scanner.next(); result != null; result = scanner.next()) {
+        System.out.println(result);
+      }
+    }
+  }
 }
 
