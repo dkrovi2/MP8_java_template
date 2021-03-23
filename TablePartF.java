54877d83e5cd6c280d74b7476ae61e2391700fd3
Sarma Krovi <sarmakdvsr@gmail.com>
2021-03-22 22:33:08 -0400

    Fix TablePartF

--- TablePartF.java
+++ TablePartF.java
@@ -14,20 +14,21 @@ public class TablePartF {
     Configuration config = HBaseConfiguration.create();
     try (HTable table = new HTable(config, "powers")) {
       Scan scan = new Scan();
-      try (ResultScanner scanner1 = table.getScanner(scan); ResultScanner scanner2 = table.getScanner(scan)) {
+      try (ResultScanner scanner1 = table.getScanner(scan);) {
         for (Result result1 = scanner1.next(); result1 != null; result1 = scanner1.next()) {
-
           String name = Bytes.toString(result1.getValue(Bytes.toBytes("professional"), Bytes.toBytes("name")));
           String power = Bytes.toString(result1.getValue(Bytes.toBytes("personal"), Bytes.toBytes("power")));
           String color = Bytes.toString(result1.getValue(Bytes.toBytes("custom"), Bytes.toBytes("color")));
-          for (Result result2 = scanner2.next(); result2 != null; result2 = scanner2.next()) {
+          try (ResultScanner scanner2 = table.getScanner(scan)) {
+            for (Result result2 = scanner2.next(); result2 != null; result2 = scanner2.next()) {
 
-            String name1 = Bytes.toString(result2.getValue(Bytes.toBytes("professional"), Bytes.toBytes("name")));
-            String power1 = Bytes.toString(result2.getValue(Bytes.toBytes("personal"), Bytes.toBytes("power")));
-            String color1 = Bytes.toString(result2.getValue(Bytes.toBytes("custom"), Bytes.toBytes("color")));
+              String name1 = Bytes.toString(result2.getValue(Bytes.toBytes("professional"), Bytes.toBytes("name")));
+              String power1 = Bytes.toString(result2.getValue(Bytes.toBytes("personal"), Bytes.toBytes("power")));
+              String color1 = Bytes.toString(result2.getValue(Bytes.toBytes("custom"), Bytes.toBytes("color")));
 
-            if (color.equals(color1) && !name.equals(name1)) {
-              System.out.println(name + ", " + power + ", " + name1 + ", " + power1 + ", " + color);
+              if (color.equals(color1) && !name.equals(name1)) {
+                System.out.println(name + ", " + power + ", " + name1 + ", " + power1 + ", " + color);
+              }
             }
           }
         }
